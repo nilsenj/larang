9d4766f1b4a8ad8eff6fa60d5433cfb0ebc0c0b3
nilsenj <nikoleivan@gmail.com>
2017-08-11 20:43:15 +0300

    bootstrap4 link added

--- public/dist/styles.bundle.js
+++ public/dist/styles.bundle.js
@@ -455,7 +455,7 @@ function updateLink(linkElement, obj) {
 
 /***/ }),
 
-/***/ 2:
+/***/ 3:
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__("../../../../bootstrap/dist/css/bootstrap.min.css");
@@ -464,5 +464,5 @@ module.exports = __webpack_require__("../../../../../src/styles.scss");
 
 /***/ })
 
-},[2]);
+},[3]);
 //# sourceMappingURL=styles.bundle.js.map
